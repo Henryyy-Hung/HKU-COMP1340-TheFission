a724d0ac0a5d8288db1939591272839b1bb2968b
Hung Ka Hing <hkh1395831786@gmail.com>
2021-06-02 17:48:35 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -295,7 +295,7 @@ int main(int argc, char *argv[])
 					{
 						count = 0;
 					}
-					else if ( player.hr.quantity == 100 && count < 1 )	
+					else if ( player.hr.quantity == 100 && count < 1 && ! game_over(player) )	
 					{
 						string warning[3];
 						warning[0] = "　极度饥饿状态！！！你的饥饿值太高了！！！";
